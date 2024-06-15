7c1cfabe22fa1128763b90889830cbdf49fd9a0d
Kalimara_Peleteiro <kalimarapeleteiro@gmail.com>
2024-06-15 07:33:00 -0300

    Testando cProfile

--- tests/KeyGen (Bilar 2014).py
+++ tests/KeyGen (Bilar 2014).py
@@ -7,6 +7,7 @@ from PseudoRandomNumberGenerator import PseudoRandomNumberGenerator
 import time
 import random
 import uuid
+import cProfile
 
 
 # Função para Vetor S
@@ -17,68 +18,71 @@ def random_list(size):
     return myList
 
 
-# Passo 01 - Número Primo e Vetor S
-counter1 = time.time()
+def main():
+    # Passo 01 - Número Primo e Vetor S
+    # counter1 = time.time()
 
-# Escolhendo Número Primo
-while True:
-    prime_number = next_prime(random.getrandbits(MEDIUM["eta"]))
+    # Escolhendo Número Primo
+    while True:
+        prime_number = next_prime(random.getrandbits(MEDIUM["eta"]))
 
-    if len(prime_number) == MEDIUM["eta"]:
-        counter1end = time.time()
-        break
+        if len(prime_number) == MEDIUM["eta"]:
+            counter1end = time.time()
+            break
 
-# Criando Vetor S
-assert MEDIUM["Theta"] % 15 == 0
-size = int(MEDIUM["Theta"]/15)
-s = [1] + [0 for i in range(size - 1)]
+    # Criando Vetor S
+    assert MEDIUM["Theta"] % 15 == 0
+    size = int(MEDIUM["Theta"]/15)
+    s = [1] + [0 for i in range(size - 1)]
 
-for i in range(15 - 1):
-    s = s + random_list(size)
-
-
-# Passo 02 - q0 e x0
-q0 = random.randint(0, mpz(2) ** MEDIUM["gam"]/prime_number)
-x0 = q0 * prime_number
+    for i in range(15 - 1):
+        s = s + random_list(size)
 
 
-# Passo 03 - Lista Delta
-value = uuid.uuid4()
-f1 = PseudoRandomNumberGenerator(seed = int(value.int), element_size=MEDIUM["gam"], list_size=MEDIUM["tau"])
-
-delta = [(chi % prime_number) + prime_number -
-                       random.randint(0, mpz(2) ** (MEDIUM["lam"]) + 
-                                      MEDIUM["eta"] * prime_number - 
-                                      random.randint(- (mpz(2) ** MEDIUM["rho"]), 
-                                                     mpz(2)**MEDIUM["rho"])) for chi in f1]
+    # Passo 02 - q0 e x0
+    q0 = random.randint(0, mpz(2) ** MEDIUM["gam"]/prime_number)
+    x0 = q0 * prime_number
 
 
-# Passo 04 - Elemento ul
-kappa = MEDIUM["gam"] + MEDIUM["eta"] + 2
-value = uuid.uuid4()
-f2 = PseudoRandomNumberGenerator(seed = int(value.int), 
-                                 element_size=kappa, list_size=MEDIUM["Theta"])
-f2[0] = 0
+    # Passo 03 - Lista Delta
+    value = uuid.uuid4()
+    f1 = PseudoRandomNumberGenerator(seed = int(value.int), element_size=MEDIUM["gam"], list_size=MEDIUM["tau"])
 
-somatorio = 0
-i = 0
-for u in f2:
-    somatorio += u * s[i]  # Multiplica o número aleatório gerado pelo índice em Vetor S
-    i += 1
-soma = mpz(round((mpz(2) ** kappa)/prime_number))
-ul = soma-somatorio
+    delta = [(chi % prime_number) + prime_number -
+                        random.randint(0, mpz(2) ** (MEDIUM["lam"]) + 
+                                        MEDIUM["eta"] * prime_number - 
+                                        random.randint(- (mpz(2) ** MEDIUM["rho"]), 
+                                                        mpz(2)**MEDIUM["rho"])) for chi in f1]
 
 
-# Passo 05 - Lista DeltaPrime
-value = uuid.uuid4()
-f3 = PseudoRandomNumberGenerator(seed = int(value.int), element_size=MEDIUM["gam"], list_size=MEDIUM["Theta"])
+    # Passo 04 - Elemento ul
+    kappa = MEDIUM["gam"] + MEDIUM["eta"] + 2
+    value = uuid.uuid4()
+    f2 = PseudoRandomNumberGenerator(seed = int(value.int), 
+                                    element_size=kappa, list_size=MEDIUM["Theta"])
+    f2[0] = 0
 
-deltaPrime = [chi % prime_number + 
-                           random.randint(0, mpz(2) ** (MEDIUM["gam"] + 
-                                                        MEDIUM["eta"]/prime_number)/prime_number) 
-                                                        * prime_number - 2 * random.randint(-mpz(2) ** MEDIUM["rho"], mpz(2) ** MEDIUM["rho"]) - si for chi, si in zip(f3, s)]
+    somatorio = 0
+    i = 0
+    for u in f2:
+        somatorio += u * s[i]  # Multiplica o número aleatório gerado pelo índice em Vetor S
+        i += 1
+    soma = mpz(round((mpz(2) ** kappa)/prime_number))
+    ul = soma-somatorio
 
 
-counter1end = time.time()
+    # Passo 05 - Lista DeltaPrime
+    value = uuid.uuid4()
+    f3 = PseudoRandomNumberGenerator(seed = int(value.int), element_size=MEDIUM["gam"], list_size=MEDIUM["Theta"])
 
-print(f"Tempo Final de Execução: {counter1end - counter1:.2f} segundos")
+    deltaPrime = [chi % prime_number + 
+                            random.randint(0, mpz(2) ** (MEDIUM["gam"] + 
+                                                            MEDIUM["eta"]/prime_number)/prime_number) 
+                                                            * prime_number - 2 * random.randint(-mpz(2) ** MEDIUM["rho"], mpz(2) ** MEDIUM["rho"]) - si for chi, si in zip(f3, s)]
+
+
+    #counter1end = time.time()
+
+    #print(f"Tempo Final de Execução: {counter1end - counter1:.2f} segundos")
+
+cProfile.run('main()')
